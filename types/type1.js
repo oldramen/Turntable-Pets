db658cbbb735acbfd367603ea241c41e5e46066d
yayramen <daltongore@gmail.com>
2012-05-17 13:34:59 -0700

    cat/dog update, thanks to axe

--- types/type1.js
+++ types/type1.js
@@ -6,14 +6,14 @@
  */
 
 global.mHungry = ["I'm hungry D:", "Can I have a treat? I'm starving!", "forget dog food can i have a bite of what you are eating?"];
-global.mSpeak = ["I'm excited to see you!", "ahwooooOOoooOOo. <.<", "GRRRRRRRRR! DON'T MAKE ME BITE.", "I may not seem tuff, but I could rip you to shreds :)"];
-global.mFed = ["That tasted way better than i thought it would!", "I usually only eat PREMIUM dog food, but that was alright :)"];
+global.mSpeak = ["I'm excited to see you!", "ahwooooOOoooOOo. <.<", "I may not seem tuff, but I could rip you to shreds :)", "meow- i mean bark."];
+global.mFed = ["That tasted way better than i thought it would!", "yumm :)", "I usually only eat PREMIUM dog food, but that was alright :)"];
 global.mBathed = ["I'm to busy for a bat-SQUIRREL"];
 global.mIdle = ["/me stretches and lays down", "/me howls", "Bark.", "i'mmmm sllleeeeeepppppy *yawns*"];
 
 global.sCommands = [{
     command: 'pet',
-    message: ["^-^", "GRRRR DON'T TOUCH ME RIGHT NOW", "/me bites hand (don't sneak up on me like that!)"],
+    message: ["^-^ Thank youuuuu :D", "<3", "/me howls (happy)", "/me bites hand (don't sneak up on me like that!"],
     callback: function (a, b) {
         if (b == '@'+mName) mSay(a, mRandom(this.message));
     },
@@ -24,7 +24,7 @@ global.sCommands = [{
 {
     command: 'eat',
     callback: function (a, b) {
-        if (a == mOwner) mSay(a, "/me noms on "+b);
+        if (a == mOwner) mSay(a, "/me chows down on "+b);
     },
     level: 0,
     mode: 1,
--- types/type2.js
+++ types/type2.js
@@ -1,15 +1,34 @@
 /**
  * @copyright 2012 yayramen. 
- * @author yayramen
+ * @author yayramen && Axe_
  * @description This is the file that contains the type commands
  * @Type: Cat
  */
- 
-global.mHungry = ["I'm hungry D:"];
-global.mSpeak = ["Ohi there :D"];
-global.mFed = ["Omnomnom!"];
-global.mBathed = ["B'awww, I hate baths!"];
 
-global.sCommands = [];
+global.mHungry = ["I'm hungry D:", "/me cateyes", "i'll just be mean until I get my food."];
+global.mSpeak = ["No.", "let me be llaaaaazzzzyyyyyy. <.<", "you cat to be kitten me right meow!", "Hamster souds good right about- I mean meow :cat:"];
+global.mFed = ["That tasted, meeeeh. it was alright", "MEOW! THAT WAS GOOD!"];
+global.mBathed = ["If you make me take a bath i'll claw your eyes out!"];
+global.mIdle = ["/me goes to sleeep. meeoww.", "I'm lonely :(", "Meow.", "*yawns*"];
+
+global.sCommands = [{
+    command: 'pet',
+    message: ["purrrrrrrrr", ":cat: (happy)", "miiieeeoowwwww.", "HISSSSS! you scared me!"],
+    callback: function (a, b) {
+        if (b == '@'+mName) mSay(a, mRandom(this.message));
+    },
+    level: 0,
+    mode: 0,
+    hint: 'Pets the cat'
+},
+{
+    command: 'eat',
+    callback: function (a, b) {
+        if (a == mOwner) mSay(a, "/me quietly chews on "+b);
+    },
+    level: 0,
+    mode: 1,
+    hint: 'the cat eats'
+}];
 
 mCommands = _.union(mCommands, sCommands);
